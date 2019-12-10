1f99dbd5b8f12faac15df9fea498b08438e1cf31
Maro Balestracci <2109711@talnet.nl>
2019-12-10 13:42:43 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -11,6 +11,9 @@ $input = $argv[1];
 $inputRounded = intval($input);
 if(!$inputRounded == 0) {
     foreach($nummers1 as $nummers2){
+        if($input == $nummers2) {
+            continue;
+        }
         $amountX = floor($inputRounded / $nummers2);
         echo "$amountX times $nummers2" . PHP_EOL;
         $minus = $amountX * $nummers2;
