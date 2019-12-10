bc448c0df8e639b45055b8424ffd21e9552d768a
Maro Balestracci <2109711@talnet.nl>
2019-12-10 11:30:01 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,23 +1,23 @@
 <?php
 $nummers1=array(
- $een=1,
- $twee=2,
- $vijf=5,
- $tien=10,
- $twintig=20,
- $vijftig=50
+    $vijftig=50,
+    $twintig=20,
+    $tien=10,
+    $vijf=5,
+    $twee=2,
+    $een=1
 );
 $input = $argv[1];
 $inputRounded = intval($input);
 if(!$inputRounded == 0) {
-foreach($nummers1 as $nummers2){
-$amountOfx = floor($inputRounded / $nummers2);
-echo "$amountOfx times $nummers2" . PHP_EOL;
-$minus = $amountOfx * $nummers2;
-$inputRounded = $inputRounded - $minus;   
-}
+    foreach($nummers1 as $nummers2){
+        $amountX = floor($inputRounded / $nummers2);
+        echo "$amountX times $nummers2" . PHP_EOL;
+        $minus = $amountX * $nummers2;
+        $inputRounded = $inputRounded - $minus;   
+    }
 } else {
-exit("> Je krijgt geen wisselgeld terug.");
+    exit("> Je krijgt geen wisselgeld terug.");
 } 
 
 echo("used heroin needle");
